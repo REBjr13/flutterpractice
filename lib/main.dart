95548b8f6e43d66fa49ed799164289ea7fa6060a
REBjr13 <rebcave8@gmail.com>
2021-08-02 17:20:44 +0100

    created seperate files for projects

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/ui/home..dart';
+//import 'package:flutter_application/ui/home.dart';
+import 'package:flutter_application/classes/VLCclone.dart';
 
-void main() => runApp( new MaterialApp(
-  home: ScaffoldExample(),
-)
-
-);
+void main() => runApp(new MaterialApp(
+      home: ScaffoldExample(),
+    ));
