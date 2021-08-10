05b9f94f338100611664427ba95260147d9c4ef6
REBjr13 <rebcave8@gmail.com>
2021-08-10 15:24:00 +0100

    Started Tip Calculator app

---
+++ lib/classes/Tipcalculator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Tipcalc extends StatefulWidget {
+  @override
+  _TipcalcState createState() => _TipcalcState();
+}
+
+class _TipcalcState extends State<Tipcalc> {
+
+  int _tipPercent = 0;
+  int _personCounter = 1;
+  double _billAmount = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
