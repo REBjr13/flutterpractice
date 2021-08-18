8de887ee7a92befd5b13f1efb0093c230b944738
REBjr13 <rebcave8@gmail.com>
2021-08-18 18:41:06 +0100

    progersson the quiz app

--- lib/classes/QuizApp.dart
+++ lib/classes/QuizApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/model/question.dart';
 import 'package:flutter_application/ui/util/hexcolor.dart';
 
 Color blue1 = HexColor("#345867");
@@ -9,6 +10,17 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List questions = [
+    Question.name("I was born in 2002?", false),
+    Question.name("Junior is part of my name?", true),
+    Question.name("I live in Abuja?", false),
+    Question.name("I have 2 brothers?", false),
+    Question.name("I am 5.8' tall?", false),
+    Question.name("I love football?", true),
+    Question.name("I love anime?", true),
+    Question.name("I love anime?", true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +32,24 @@ class _QuizState extends State<Quiz> {
       ),
       backgroundColor: Colors.blueGrey,
       body: Container(
-        child: Column(
-          children: [
-            Image.asset("images/reb.jpeg"),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                  child:
+                      Image.asset("images/coat.jpeg", height: 280, width: 180)),
+              Container(
+                height: 120.0,
+                child: Center(
+                  child: Text(questions[0],
+                      style: TextStyle(
+                          fontSize: 14.0, fontWeight: FontWeight.bold)),
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
         ),
       ),
     );
