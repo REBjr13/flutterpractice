3156250db2f541cb2a9c41d1d0ec223b3e9bf5e7
REBjr13 <rebcave8@gmail.com>
2022-04-07 15:26:37 +0100

    shaping the UI

--- lib/classes/MovieApp.dart
+++ lib/classes/MovieApp.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application/classes/page.dart';
 
@@ -32,35 +34,111 @@ class Movie extends StatelessWidget {
         body: ListView.builder(
             itemCount: movieList.length,
             itemBuilder: (BuildContext context, int index) {
-              return Card(
-                elevation: 5.0,
-                color: Colors.blue,
-                child: ListTile(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MovieListDetails(
-                                    movieName: movieList[index].title,
-                                movie: movieList[index],
-                                  )));
-                    },
-                    // onTap: ()=> {
-                    //   debugPrint('${anime.elementAt(index)}'),
-                    // },
-                    title: Text(movieList[index].title),
-                    subtitle: Text('${movieList[index].title}'),
-                    trailing: Text('....'),
-                    leading: CircleAvatar(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            //color: Colors.blue,
-                            borderRadius: BorderRadius.circular(60)),
-                        child: Text('B'),
-                      ),
-                      backgroundColor: Colors.grey,
-                    )),
-              );
+              return Stack(children: [
+                movieCard(movieList[index], context),
+                Positioned(
+                    top: 10, child: movieImage(movieList[index].images[2])),
+              ]);
+              // return Card(
+              //    elevation: 5.0,
+              //    color: Colors.blue,
+              //    child: ListTile(
+              //        onTap: () {
+              //          Navigator.push(
+              //              context,
+              //              MaterialPageRoute(
+              //                  builder: (context) => MovieListDetails(
+              //                        movieName: movieList[index].title,
+              //                    movie: movieList[index],
+              //                      )));
+              //        },
+              //        // onTap: ()=> {
+              //        //   debugPrint('${anime.elementAt(index)}'),
+              //        // },
+              //        title: Text(movieList[index].title, style: TextStyle(fontWeight: FontWeight.bold)),
+              //        subtitle: Text('${movieList[index].plot}'),
+              //        trailing: Text('....'),
+              //        leading: CircleAvatar(
+              //          child: Container(
+              //            width: 200,
+              //            height : 200,
+              //            decoration: BoxDecoration(
+              //              image: DecorationImage(
+              //               image: NetworkImage(movieList[index].images[2]), fit: BoxFit.fill
+              //              ),
+              //                borderRadius: BorderRadius.circular(60)),
+              //            child: null,
+              //          ),
+              //          backgroundColor: Colors.grey,
+              //        )),
+              //  );
             }));
+  }
+
+  Widget movieCard(Movies movie, BuildContext context) {
+    return InkWell(
+      child: Container(
+        margin: EdgeInsets.only(left: 70.0),
+        width: MediaQuery.of(context).size.width,
+        height: 120.0,
+        child: Card(
+          color: Colors.black54,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 54),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(movie.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.white,
+                      ),),
+                      Text("Rating: ${movie.imdbRatings} / 10", style: TextStyle(
+                        fontSize: 17, color: Colors.grey
+                      ),)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("Released: ${movie.released}"),
+                      Text("${movie.runtime}"),
+                      Text("${movie.rated}")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MovieListDetails(
+                      movieName: movie.title,
+                      movie: movie,
+                    )));
+      },
+    );
+  }
+
+  Widget movieImage(String imageUrl) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+              image: NetworkImage(imageUrl ?? ''), fit: BoxFit.cover)),
+    );
   }
 }
