24ca348ce9c409f85d7a533cb836507ce50a2496
REBjr13 <rebcave8@gmail.com>
2022-04-27 14:43:22 +0100

    adding more details to the routes

--- lib/classes/page.dart
+++ lib/classes/page.dart
@@ -21,7 +21,8 @@ class MovieListDetails extends StatelessWidget {
       body: ListView(
         children: [
           MovieDetailsThumbnail(thumbnail: movie.images[2]),
-          MovieDetailsHeaderWithPoster(movie: movie)
+          MovieDetailsHeaderWithPoster(movie: movie),
+          MovieDetailsCast(movie: movie,)
         ],
       ),
       // body: Center(
@@ -158,5 +159,52 @@ class MovieListDetailsHeader extends StatelessWidget {
                 ))
           ]))
     ]);
+  }
+}
+
+class MovieDetailsCast extends StatelessWidget {
+  final Movies movie;
+
+  const MovieDetailsCast({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          MovieField(field: 'Cast', value: movie.actors),
+          MovieField(field: 'Directors', value: movie.director),
+          MovieField(field: 'awards', value: movie.awards),
+        ],
+      ),
+    );
+  }
+}
+
+class MovieField extends StatelessWidget {
+  final String field;
+  final String value;
+
+  const MovieField({Key key, this.field, this.value}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$field : ',
+          style: TextStyle(color: Colors.black54, fontWeight: FontWeight.w300),
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: TextStyle(color: Colors.black54),
+          ),
+        )
+      ],
+    );
   }
 }
