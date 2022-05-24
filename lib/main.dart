11165a5d9d292eef04831b919be1472011cd05c6
REBjr13 <rebcave8@gmail.com>
2022-05-25 00:55:33 +0100

    theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+
+
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:flutter_application/classes/QuizApp.dart';
+import 'package:flutter_application/ui/mortgage_app.dart';
+import 'package:flutter_application/ui/util/colors.dart';
 
 // ignore: unused_import
 import 'ui/movie_ui/MovieApp.dart';
@@ -8,29 +12,41 @@ import 'ui/movie_ui/MovieApp.dart';
 final ThemeData _appTheme = _buildTheme();
 
 ThemeData _buildTheme() {
-  final ThemeData base = ThemeData.dark();
+  final ThemeData base = ThemeData.light();
   return base.copyWith(
-    brightness: Brightness.dark,
-    scaffoldBackgroundColor: Colors.grey,
-    primaryColor: Colors.lightGreen,
-    backgroundColor: Colors.blueGrey,
-    textTheme: _appTextTheme(base.textTheme),
-  );
+      //appBarTheme: _appBarTheme,
+      textTheme: _appTextTheme(base.textTheme),
+      colorScheme: _appTheme.colorScheme.copyWith(secondary: primaryGreenDark),
+      primaryColor: primaryGreen900,
+      scaffoldBackgroundColor: secondaryBackgroundWhite,
+      hintColor: textOnPrimary,
+      cardColor: secondaryLGreenDark,
+      inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: primaryGreenLight),
+              borderRadius: BorderRadius.circular(14)
+              )));
 }
 
-TextTheme _appTextTheme(TextTheme base) {
+// AppBarTheme _appBarTheme = AppBarTheme(
+//   elevation: 10,
+//   centerTitle: true,
+// );
+
+_appTextTheme(TextTheme base) {
   return base
       .copyWith(
         headline1: base.headline1.copyWith(
           fontWeight: FontWeight.w600,
         ),
-        bodyText2: base.bodyText1.copyWith(
+        bodyText2: TextStyle(
           fontWeight: FontWeight.bold,
-          fontSize: 17,
-          color: Colors.black38,
+          fontSize: 15,
+          color: textOnPrimary,
           letterSpacing: 1.0,
           fontFamily: "HinaMincho",
         ),
+        bodyText1: TextStyle(fontSize: 15, fontFamily: "HinaMincho"),
         button: base.button.copyWith(
           fontSize: 14,
           letterSpacing: 3.0,
@@ -47,5 +63,5 @@ TextTheme _appTextTheme(TextTheme base) {
 void main() => runApp(new MaterialApp(
       theme: _appTheme,
       debugShowCheckedModeBanner: true,
-      home: Quiz(),
+      home: MortgageApp(),
     ));
--- lib/ui/util/colors.dart
+++ lib/ui/util/colors.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 //color palette used: https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=9FA8DA
 
-const primaryIndigo200 = const Color(0xFF9FA8DA);
-const primaryIndigoLight = const Color(0xFFd1d9ff);
-const primaryIndigoDark = const Color(0xff6f79a8);
+const primaryGreen900 = const Color(0xFF1b5e20);
+const primaryGreenLight = const Color(0xFF4c8c4a);
+const primaryGreenDark = const Color(0xff003300);
 
-const textOnPrimary = const Color(0xff000000);
+const secondaryLGreen400 = const Color(0xFF9ccc65);
+const secondaryLGreenLight = const Color(0xFFcfff95);
+const secondaryLGreenDark = const Color(0xff6b9b37);
+
+const textOnPrimary = const Color(0xffffffff);
+const textOnSecondary = const Color(0xff000000);
 const secondaryBackgroundWhite = const Color(0xffffffff);
